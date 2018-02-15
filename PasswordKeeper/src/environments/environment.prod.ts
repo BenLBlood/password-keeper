9b68fde5e011d01c79e0a6b1e508aea194bec421
Ben Blood <bluealikai@gmail.com>
2018-02-14 23:41:57 -0500

    Password keeper initial setup

--- PasswordKeeper/src/environments/environment.prod.ts
+++ PasswordKeeper/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmYn7TRz8BjRdqmqM8zHJ2ud1riMzqOBU",
+    authDomain: "bblood-password-keeper.firebaseapp.com",
+    databaseURL: "https://bblood-password-keeper.firebaseio.com",
+    projectId: "bblood-password-keeper",
+    storageBucket: "bblood-password-keeper.appspot.com",
+    messagingSenderId: "25225255815"
+  }
 };
--- PasswordKeeper/src/environments/environment.ts
+++ PasswordKeeper/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmYn7TRz8BjRdqmqM8zHJ2ud1riMzqOBU",
+    authDomain: "bblood-password-keeper.firebaseapp.com",
+    databaseURL: "https://bblood-password-keeper.firebaseio.com",
+    projectId: "bblood-password-keeper",
+    storageBucket: "bblood-password-keeper.appspot.com",
+    messagingSenderId: "25225255815"
+  }
 };
